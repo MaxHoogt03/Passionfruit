77e5dac91c7f33ef6ea973187e9408b15220dfc0
MaxHoogt03 <max.hoogeveen@student.uva.nl>
2024-01-10 13:18:26 +0100

    made the reading of files object-oriented

--- calc.py
+++ calc.py
@@ -1,27 +1,9 @@
 import csv
 
-from models import Battery, House
+from models import Battery, House, read_battery, read_house
 
-district_1_batteries = []
-district_1_houses = []
-
-with open('data/district_1/district-1_batteries.csv', 'r') as file:
-    csv_reader = csv.DictReader(file)
-
-    for row in csv_reader:
-        x, y = row['positie'].split(',')
-        capacity = row['capaciteit']
-        district_1_batteries.append(Battery(x, y, capacity))
-
-with open('data/district_1/district-1_houses.csv', 'r') as file:
-    csv_reader = csv.DictReader(file)
-
-    for row in csv_reader:
-        x = row['x']
-        y = row['y']
-        maxoutput = row['maxoutput']
-        district_1_houses.append(House(x, y, maxoutput))
-
+district_1_batteries = read_battery('district-1_batteries.csv')
+district_1_houses = read_house('district-1_houses.csv')
 
 def calculate_distance(point1, point2):
     return abs(point1.x - point2.x) + abs(point1.y - point2.y)
