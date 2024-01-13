9273248c6fa3d574e2b866b12bdd8f392f53f451
MaxHoogt03 <max.hoogeveen@student.uva.nl>
2024-01-13 18:12:27 +0100

    Guaranteed result for randomized, but still needs some cleaning

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -10,56 +10,67 @@ def randomize_houses(district):
     random.shuffle(district.get_houses())
 
 def random_solution(district):
-    randomize_houses(district)
+    retry = True
+    while retry:
+        randomize_houses(district)
 
-    total_min_distance = 0
-    for house in district.houses:
-        smallest_distance = None
+        total_min_distance = 0
+        retry = False  # Reset the retry flag at the beginning of each attempt
+        for house in district.houses:
+            smallest_distance = None
+            closest_battery = None
 
-        # loops over all batteries and checks which battery is closest to current house, with its distance.
-        for battery in district.batteries:
-            distance = District.calculate_distance(house, battery)
+            # loops over all batteries and checks which battery is closest to current house, with its distance.
+            for battery in district.batteries:
+                distance = District.calculate_distance(house, battery)
+                
+                if (smallest_distance is None or distance < smallest_distance) and battery.get_capacity() >= house.get_output():
+                    smallest_distance = distance
+                    closest_battery = battery
             
-            if (smallest_distance is None or distance < smallest_distance) and battery.get_capacity() >= house.get_output():
-                smallest_distance = distance
-                closest_battery = battery
+            if closest_battery is not None:
+                # Variables for location of current house, closest battery, and the distance.
+                x_house = house.x
+                y_house = house.y
+                x_battery = closest_battery.x
+                y_battery = closest_battery.y
+                dist_x = x_house - x_battery
+                dist_y = y_house - y_battery
 
-        # Variables for location of current house, closest battery and the distance.
-        x_house = house.x
-        y_house = house.y
-        x_battery = closest_battery.x
-        y_battery = closest_battery.y
-        dist_x = x_house - x_battery
-        dist_y = y_house - y_battery
+                # Adds the cables to the houses, by first walking over the x difference and then the y difference.
+                if dist_x < 0:
+                    for i in range(abs(dist_x)):
+                        house.add_cable(f"{x_house + i}, {y_house}")
+                
+                elif dist_x > 0:
+                    for i in range(dist_x):
+                        house.add_cable(f"{x_house-i}, {y_house}")
 
-        # Adds the cables to the houses, by first walking over the x difference and then the y difference.
-        if dist_x < 0:
-            for i in range(abs(dist_x)):
-                house.add_cable(f"{x_house + i}, {y_house}")
-        
-        elif dist_x > 0:
-            for i in range(dist_x):
-                house.add_cable(f"{x_house-i}, {y_house}")
+                if dist_y < 0:
+                    for i in range(abs(dist_y) + 1):
+                        house.add_cable(f"{x_house - dist_x}, {y_house + i}")
+                
+                elif dist_y > 0:
+                    for i in range(dist_y + 1):
+                        house.add_cable(f"{x_house-dist_x}, {y_house - i}")
 
-        if dist_y < 0:
-            for i in range(abs(dist_y) + 1):
-                house.add_cable(f"{x_house - dist_x}, {y_house + i}")
-        
-        elif dist_y > 0:
-            for i in range(dist_y + 1):
-                house.add_cable(f"{x_house-dist_x}, {y_house - i}")
+                # Adds all distances of the houses to their closest battery.
+                total_min_distance += smallest_distance
 
-        # Adds all distances of the houses to their closest battery.
-        total_min_distance += smallest_distance
+                # Retract the output from the house from the capacity of the battery.
+                closest_battery.retract_capacity(house.get_output())
 
-        # Retract the output from the house from the capacity of the battery.
-        closest_battery.retract_capacity(house.get_output())
-        print(closest_battery)
-
-        # Adds to house to the current battery object.
-        closest_battery.add_house(house)
-
+                # Adds to house to the current battery object.
+                closest_battery.add_house(house)
+            else:
+                # If no suitable battery is found for a house, set the retry flag and break the loop
+                print("Retrying")
+                retry = True
+                for battery in district.batteries:
+                    battery.capacity = 1507
+                break
 
     district.output()
     district.plot_cables()
+
 
--- main.py
+++ main.py
@@ -4,9 +4,4 @@ from code.algorithms import randomise
 
 if __name__ == "__main__":
     district_1 = district.District(1, "data/district_1/district-1_")
-    sum = 0
-    for item in district_1.houses:
-        sum += item.get_output()
-
-    print(sum)
     randomise.random_solution(district_1)
